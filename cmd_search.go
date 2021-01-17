6e42e7cea128ccbacc6450bd8c87a7248d56a454
Tong Sun <suntong@cpan.org>
2021-01-17 12:03:42 -0500

    - [+] borrow the default behavior of SearchResult.String

--- cmd_search.go
+++ cmd_search.go
@@ -43,6 +43,51 @@ func DoSearch(idx bleve.Index, query string) error {
 	sreq.Highlight = bleve.NewHighlightWithStyle("ansi")
 	sres, err := idx.Search(sreq)
 	clis.AbortOn("Doing Search", err)
-	fmt.Println(sres)
+	fmt.Println(searchResult{sres})
 	return nil
+}
+
+type searchResult struct {
+	*bleve.SearchResult
+}
+
+func (sr searchResult) String() string {
+	rv := ""
+	if sr.Total > 0 {
+		if sr.Request.Size > 0 {
+			rv = fmt.Sprintf("%d matches, showing %d through %d, took %s\n", sr.Total, sr.Request.From+1, sr.Request.From+len(sr.Hits), sr.Took)
+			for i, hit := range sr.Hits {
+				rv += fmt.Sprintf("%5d. %s (%f)\n", i+sr.Request.From+1, hit.ID, hit.Score)
+				for fragmentField, fragments := range hit.Fragments {
+					rv += fmt.Sprintf("\t%s\n", fragmentField)
+					for _, fragment := range fragments {
+						rv += fmt.Sprintf("\t\t%s\n", fragment)
+					}
+				}
+				for otherFieldName, otherFieldValue := range hit.Fields {
+					if _, ok := hit.Fragments[otherFieldName]; !ok {
+						rv += fmt.Sprintf("\t%s\n", otherFieldName)
+						rv += fmt.Sprintf("\t\t%v\n", otherFieldValue)
+					}
+				}
+			}
+		} else {
+			rv = fmt.Sprintf("%d matches, took %s\n", sr.Total, sr.Took)
+		}
+	} else {
+		rv = "No matches"
+	}
+	if len(sr.Facets) > 0 {
+		rv += fmt.Sprintf("Facets:\n")
+		for fn, f := range sr.Facets {
+			rv += fmt.Sprintf("%s(%d)\n", fn, f.Total)
+			for _, t := range f.Terms {
+				rv += fmt.Sprintf("\t%s(%d)\n", t.Term, t.Count)
+			}
+			if f.Other != 0 {
+				rv += fmt.Sprintf("\tOther(%d)\n", f.Other)
+			}
+		}
+	}
+	return rv
 }
